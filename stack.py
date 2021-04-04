b6b1688249b61d0de621de45a2ac9feb0c235159
SumirVats2003 <65482161+SumirVats2003@users.noreply.github.com>
2021-04-04 22:40:48 +0530

    Add files via upload

---
+++ stack.py
@@ -0,0 +1,47 @@
+# stacks
+
+# 'PUSH', 'POP', 'PEAK', 'DISPLAY'
+
+l1 = []
+
+def push(lst, value):
+    lst.append(value)
+    print("Element pushed successfully")
+
+
+def pop(lst):
+    p = len(lst)
+    if p !=0:
+        a = lst[p-1]
+        del lst[p-1]
+        print("Popped element:",a)
+    else:
+        print("There is no element in list")
+
+def peak(lst):
+    p = len(lst)
+    if p != 0:
+        element = lst[p-1]
+        print(element)
+    else:
+        print("Empty List")
+
+def display(lst):
+    print(lst)
+
+while True:
+    print("PUSH:1, POP:2, PEAK:3, DISPLAY:4, EXIT:5")
+    action = int(input("Enter action:"))
+    if action == 1:
+        val = int(input("Enter value to push: "))
+        push(l1,val)
+    elif action == 2:
+        pop(l1)
+    elif action == 3:
+        peak(l1)
+    elif action == 4:
+        display(l1)
+    elif action == 5:
+        break
+    else:
+        print("Please enter valid input")
