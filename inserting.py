8d656a4c97e27b406411b38e1d637057238b9886
SumirVats2003 <65482161+SumirVats2003@users.noreply.github.com>
2021-04-04 20:28:31 +0530

    Add files via upload

---
+++ inserting.py
@@ -0,0 +1,23 @@
+l1 = []
+
+def insert(number, iList):
+    for i in iList:
+        if number < i:
+            idx = iList.index(i)
+            iList.insert(idx, number)
+            break
+        elif number > i:
+            pass
+        else:
+            print("Element already in list")
+    else:
+        iList.append(number)
+    print(iList)
+
+
+ind = int(input("Enter number of entries: "))
+for j in range(ind):
+    p = int(input('Enter element: '))
+    l1.append(p)
+num = int(input("Enter number to insert: "))
+insert(num, l1)
